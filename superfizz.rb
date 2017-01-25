011c06b66a028d17553f29bb506ad5857f9254d9
Bailey Ward <bailey.wam@gmail.com>
2017-01-24 19:19:46 -0700

    forgot to save

--- superfizz.rb
+++ superfizz.rb
@@ -1,1 +1,33 @@
-(0...1000).each 
+(0..1000).each do |num|
+  if num % 3 == 0 &&
+    num % 5 == 0 &&
+    num % 7 == 0
+    puts "#{num}: SuperfizzzBuzz"
+
+  elsif num % 3 == 0 &&
+      num % 7 == 0
+      puts "#{num}: Superfizz"
+
+  elsif  num % 5 == 0 &&
+        num % 7 == 0
+        puts "#{num}: SuperBuzz"
+        
+  elsif num % 3 == 0 &&
+        num % 5 == 0
+        puts "#{num}: FizzBuzz"
+
+  elsif num % 3 == 0
+        puts "#{num}: Fizz"
+
+  elsif num % 5 == 0
+        puts "#{num}: Buzz"
+
+  elsif num % 7 == 0
+        puts "#{num}: Super"
+
+  else
+    puts "#{num}"
+
+end
+
+end
